30e4d50558a580001d18e5c3221affd5c627a05e
Nitsan Avni <nitsan@papayaglobal.com>
2019-06-08 20:43:40 +0300

    working

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,8 @@ export interface Config {
     matchProportionBonus?: number | boolean;
     smartCase?: number | boolean;
     allowWordsOutOfOrder: number | boolean;
+    markBefore?: string;
+    markAfter?: string;
 }
 
 type Index = number;
@@ -18,6 +20,7 @@ export type Span = [From, To] | Index;
 export interface Match {
     spans: Span[];
     score: number;
+    marked: string;
 }
 
 interface ConfigNumbers {
@@ -25,13 +28,17 @@ interface ConfigNumbers {
     startOfWordBonus: number;
     allowErrors: number;
     matchProportionBonus: number;
+    markBefore: string;
+    markAfter: string;
 }
 
 const defaultConfig: ConfigNumbers = {
     startOfStringBonus: 1,
     startOfWordBonus: 1,
     allowErrors: 0,
-    matchProportionBonus: 0
+    matchProportionBonus: 0,
+    markBefore: "*",
+    markAfter: "*"
 };
 
 export class Search {
@@ -51,15 +58,21 @@ export class Search {
 
         if (startOfStringMatch) {
             const spans: Span[] = [[startOfStringMatch.index, startOfStringMatch.index + searchTerm.length]];
+            const marked = this.mark(str, spans);
 
             return {
                 score: 3,
-                spans
+                spans,
+                marked
             };
         }
         score += re(`\\b${searchTerm}`).test(str) ? this.config.startOfWordBonus : 0;
         score += re(searchTerm).test(str) ? 1 : 0;
 
-        return { score, spans: [] };
+        return { score, spans: [], marked: str };
+    }
+
+    private mark(str: string, spans: Span[]) {
+        return str;
     }
 }
